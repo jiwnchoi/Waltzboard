444f10ecc300f622a1b08ff932482554464ce592
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-02-13 17:58:21 +0900

    [ADD] interesting statistics

--- src/oracle/Interestingness.py
+++ src/oracle/Interestingness.py
@@ -2,8 +2,10 @@ from typing import TYPE_CHECKING
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.neighbors import LocalOutlierFactor
+from scipy.stats import chi2_contingency
+from sklearn.linear_model import LogisticRegression
 
 if TYPE_CHECKING:
     from ..space.Node import VISNode
@@ -18,13 +20,10 @@ def get_outlierness_n(series: pd.Series, thresold: float = 1.5) -> float:
 
 # Q
 def get_outlierness_q(series: pd.Series) -> float:
-    q1 = series.quantile(0.25)
-    q3 = series.quantile(0.75)
-    iqr = q3 - q1
-    lower_bound = q1 - iqr * iqr
-    upper_bound = q3 + iqr * iqr
-    outliers = (series < lower_bound) | (series > upper_bound)
-    return len(outliers) / len(series)
+    lof = LocalOutlierFactor()
+    pred = lof.fit_predict(series.to_numpy().reshape(-1, 1))
+    outliers = np.count_nonzero(pred == -1)
+    return outliers / len(pred)
 
 
 def get_skewness_q(series: pd.Series) -> float:
@@ -39,24 +38,56 @@ def get_dispursion_q(series: pd.Series) -> float:
     return series.std() / series.mean()
 
 
+## NN
+def get_correlation_nn(series1: pd.Series, series2: pd.Series) -> float:
+    # cramers_v
+    confusion_matrix = pd.crosstab(series1, series2)
+    chi2 = chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2 / n
+    r, k = confusion_matrix.shape
+    phi2corr = max(0, phi2 - ((k - 1) * (r - 1)) / (n - 1))
+    rcorr = r - ((r - 1) ** 2) / (n - 1)
+    kcorr = k - ((k - 1) ** 2) / (n - 1)
+    return np.sqrt(phi2corr / min((kcorr - 1), (rcorr - 1)))
+
+
+def get_outlierness_nn(series1: pd.Series, series2: pd.Series) -> float:
+    # Point-wise Mutual Information
+    crosstab = pd.crosstab(series1, series2)
+    crosstab = crosstab.apply(lambda x: x / x.sum(), axis=1)
+    return max(np.log(crosstab) - np.log(crosstab.sum(axis=0)), 0)
+
+
 # QQ
 def get_correlation_qq(series1: pd.Series, series2: pd.Series) -> float:
-    # return perason correlation
     return series1.corr(series2)
 
 
-def get_outlierness_qq(series1: pd.Series, series2: pd.Series, k: int = 3) -> float:
-    data = pd.concat([series1, series2], axis=1)
-    X_std = StandardScaler().fit_transform(data)
-    k_means = KMeans(n_clusters=k)
-    k_means.fit(X_std)
-    distances = k_means.transform(X_std)
-    min_distances = np.min(distances, axis=1)
-    thresold = 3 * np.std(min_distances)
-    return np.sum(min_distances > thresold) / len(min_distances)
+def get_outlierness_qq(series1: pd.Series, series2: pd.Series) -> float:
+    # use lof
+    lof = LocalOutlierFactor()
+    scores = lof.fit_predict(pd.concat([series1, series2], axis=1))
+    return np.count_nonzero(scores == -1) / len(scores)
 
 
 ## QN
+def get_correlation_qn(q_series: pd.Series, n_series: pd.Series) -> float:
+    # logistic regression
+    model = LogisticRegression(solver="lbfgs")
+    model.fit(q_series, n_series)
+    pred = model.predict(q_series)
+    residuals = pred - n_series
+    return np.corrcoef(residuals, pred)[0, 1]
+
+
+def get_outlierness_qn(q_series: pd.Series, n_series: pd.Series) -> float:
+    # use lof, encode n_series one-hot
+    encoder = OneHotEncoder()
+    n_train = encoder.fit_transform(n_series)
+    lof = LocalOutlierFactor()
+    scores = lof.fit_predict(pd.concat([q_series, n_train], axis=1))
+    return np.count_nonzero(scores == -1) / len(scores)
 
 
 def get_interestingness_from_nodes(nodes: list["VISNode"], df: pd.DataFrame) -> float:
