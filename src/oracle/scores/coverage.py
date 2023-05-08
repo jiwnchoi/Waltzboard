c734485ce25c35556e8683189609f10b8867dc81
jason-choi-skku <jasonchoi3@g.skku.edu>
2023-05-08 17:33:21 +0900

    [FIX] case sensitiveness

---
+++ src/oracle/scores/coverage.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from src.model import GleanerChart
+
+
+def get_coverage_from_nodes(nodes: list["GleanerChart"], df: pd.DataFrame) -> float:
+    n = len(nodes)
+    coverages = [node.get_coverage(df) for node in nodes]
+    full_coverage = {key: 0.0 for key in list(df.columns)}
+    for coverage in coverages:
+        for key, value in coverage.items():
+            full_coverage[key] = min(1, full_coverage[key] + value)
+    return sum(full_coverage.values()) / (len(full_coverage))
